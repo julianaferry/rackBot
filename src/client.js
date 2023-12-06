ef0c2e75db795696ae6242d5b11057a133f45a8e
Juliana Ferreira <juliana.ferreira@autotrader.co.uk>
2023-12-06 14:45:24 +0000

    added

--- src/client.js
+++ src/client.js
@@ -1,11 +1,5 @@
-/* eslint-env browser */
-/* global PARTYKIT_HOST */
 
 import "./styles.css";
-
-import PartySocket from "partysocket";
-
-let pingInterval;
 
 /** @type {HTMLDivElement} - The DOM element to append all messages we get */
 const output = document.getElementById("app");
@@ -19,79 +13,14 @@ function add(text) {
   output.appendChild(document.createElement("br"));
 }
 
+// buttonArray = document.querySelectorAll('botStyle');
 
-/**
- * A PartySocket is like a WebSocket, but with more features.
- * It handles reconnection logic, buffering messages while it's offline, etc.
- * @type {PartySocket} - The connection object
- */
-const conn = new PartySocket({
-  host: PARTYKIT_HOST,
-  room: "my-new-room",
-});
+// for (let i = 0; i < buttonArray.length; i++) {
 
-/**
- * Event listener to handle received messages.
- * @param {Event} event - The message event
- */
-conn.addEventListener("message", function (event) {
-  add(`Received -> ${event.data}`);
-});
-
-/**
- * Event listener for when the connection opens.
- */
-
-const getBot1 = document.getElementById('bot1')
-getBot1.addEventListener('click', () => {
-
-  pingInterval = setTimeout(function () {
-    conn.addEventListener("message", function (event) {
-        add(`Forward -> ${event.data}`);
-      });
-
-    add('Forward');
-   
-  }, 900);
-
-});
-
-const getBot2 = document.getElementById('bot2')
-getBot2.addEventListener('click', () => {
-
-  pingInterval = setTimeout(function () {
-
-    add('Backward');
-   
-  }, 500);
-
-});
-const getBot3 = document.getElementById('bot3')
-getBot3.addEventListener('click', () => {
-
-  pingInterval = setTimeout(function () {
-
-    add('Right');
-   
-  }, 500);
-
-});
-
-const getBot4 = document.getElementById('bot4')
-getBot4.addEventListener('click', () => {
-
-  pingInterval = setTimeout(function () {
-
-    add('Left');
-   
-  }, 500);
-
-});
-
-getBot2.removeEventListener()
-getBot3.removeEventListener()
-
-
+//   h1.innerText = 'Hello World!'
+//   // Append it to the body
+//   document.body.append(h1)
+// }
 
 
 
